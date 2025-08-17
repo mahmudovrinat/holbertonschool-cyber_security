e62eed4e9e71a33369a72c192f54298d53fb59fb
Rinat <mahmudovrinat88@gmail.com>
2025-08-17 14:44:11 -0400

    task

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-whois $1 | awk '/^(Registrant|Admin|Tech)/ {gsub(/^[ \t]+|[ \t]+$/, "", $0); split($0,a,":"); gsub(/^ /,"",a[2]); print a[1]","a[2]}' > $1.csv
+# Script: 0-whois.sh
+# Usage: ./0-whois.sh <domain>
+# Description: Run whois and extract Registrant, Admin, and Tech info into CSV.
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <domain>"
+    exit 1
+fi
+
+domain="$1"
+outfile="${domain}.csv"
+
+whois "$domain" | awk -F': ' '
+/^Registrant|^Admin|^Tech/ {
+    gsub(/ /, "$", $1)   # Replace spaces in keys with $
+    gsub(/^ +| +$/, "", $2) # Trim spaces
+    print $1 "," $2
+}' > "$outfile"

c63fa4b91eafa0c2edd3e125fe27f1b0fe519d1d
Rinat <mahmudovrinat88@gmail.com>
2025-10-27 13:36:05 -0400

    Task

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-iptables -F && iptables -A INPUT -i lo -j ACCEPT && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -j DROP
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -j DROP

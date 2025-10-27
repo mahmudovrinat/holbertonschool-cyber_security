e7f91b3604fb10d80ea1c95a153338cfbd585131
Rinat <mahmudovrinat88@gmail.com>
2025-10-27 13:20:57 -0400

    Task

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -0002 ! -path "/proc/*" ! -path "/sys/*" ! -path "/run/*" 2>/dev/null -exec echo {} \; -exec chmod o-w {} \;
+find / -type d -perm -0002 ! -path "/proc/*" ! -path "/sys/*" ! -path "/run/*" 2>/dev/null -print -exec chmod o-w {} \;

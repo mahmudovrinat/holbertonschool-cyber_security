afd43ddee3d2830bd61c405f896a58f85f90df4f
Rinat <mahmudovrinat88@gmail.com>
2025-12-05 03:25:49 -0500

    Task

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -2,8 +2,8 @@
 
 LOGFILE="./auth.log"
 
-# Count unique IPs that succeeded
-grep "Accepted password" "$LOGFILE" \
+# Count unique IPs that successfully logged into the compromised account 'root'
+grep "Accepted password for root" "$LOGFILE" \
     | awk '{for(i=1;i<=NF;i++) if ($i=="from") print $(i+1)}' \
     | sort -u \
     | wc -l

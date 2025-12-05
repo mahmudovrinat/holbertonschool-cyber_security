6c9a5346f00ff63198031cdcd92806a8a2bc421e
Rinat <mahmudovrinat88@gmail.com>
2025-12-05 03:27:37 -0500

    Task

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -2,7 +2,4 @@
 
 LOGFILE="./auth.log"
 
-# Count all firewall rule additions
-grep -Ei "firewall|iptables" "$LOGFILE" \
-    | grep -Ei "add|added|insert|append" \
-    | wc -l
+grep -Ei "rule" "$LOGFILE" | grep -Ei "add" | wc -l

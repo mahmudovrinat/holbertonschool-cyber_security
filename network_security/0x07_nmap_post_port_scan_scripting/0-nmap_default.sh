77d7b3aaf32d7ae6bc188228d86a97ced069e484
Rinat <mahmudovrinat88@gmail.com>
2025-12-05 04:26:07 -0500

    Task

--- network_security/0x07_nmap_post_port_scan_scripting/0-nmap_default.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/0-nmap_default.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-
-# Check if an argument was provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <target-host>"
-    exit 1
-fi
-
-TARGET="$1"
-
-echo "Running Nmap default script scan on: $TARGET"
-echo "--------------------------------------------"
-
-# Run Nmap with default scripts
-sudo nmap -sC -sV "$TARGET"
+sudo nmap -sC -sV "$1"
